03107c04e5a8e817553a8a6c97ed89b81f21ab09
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-07 08:28:40 +0100

    Change logging level from WARN to ERROR

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from src.task_manager import BackgroundTaskManager
 
 # Configure logging
 logging.basicConfig(
-    level=logging.WARN,
+    level=logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
